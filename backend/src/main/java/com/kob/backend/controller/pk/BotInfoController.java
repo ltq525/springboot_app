781724aa5a28ff42715e5587516e6f19a4ab7c6f
quann <1766445410@qq.com>
2024-01-13 18:25:49 +0800

    骨架搭建

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -1,13 +1,23 @@
 package com.kob.backend.controller.pk;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
     @RequestMapping("/getbotinfo/")
-    public String getBotinfo() {
-        return "6666";
+    public Map<String, String> getBotinfo() {
+        Map<String, String> map = new HashMap<>();
+
+        map.put("name", "alice");
+        map.put("rating", "1500");
+        return map;
+
+
     }
 }
