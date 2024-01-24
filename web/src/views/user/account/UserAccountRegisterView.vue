64608149730afdaa6202a1497a394048a5e81284
quann <1766445410@qq.com>
2024-01-24 23:59:36 +0800

    配置mysql与注册登陆模块

---
+++ web/src/views/user/account/UserAccountRegisterView.vue
@@ -0,0 +1,16 @@
+<template>
+    <ContentField>注册</ContentField>
+</template>
+
+<script>
+import ContentField from '@/components/ContentField'
+
+export default {
+    components: {
+        ContentField,
+    }
+}
+
+</script>
+
+<style scoped></style>
